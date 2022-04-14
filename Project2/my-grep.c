bce0f09de7f448ad4c90425dcc3a4624e0a5ecc8
tuomasmustakallio <tuoppi38@gmail.com>
2022-04-14 22:04:20 +0300

    Fix: my-grep now reads multiple files.

--- Project2/my-grep.c
+++ Project2/my-grep.c
@@ -2,11 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+void read_file(FILE *file, char *search){
+
+    char *line = NULL;
+    size_t len = 0;
+
+    //Go through each line of the file and find the search term
+    while(getline(&line, &len, file) != -1){
+        if(strstr(line, search)){
+            printf("%s", line);
+        }
+    }
+
+    //Close file and free used memory
+    fclose(file);
+    free(line);
+}
+
 int main(int argc, char *argv[]){
 
     FILE *file;
-    char *line = NULL;
-    size_t len = 0;
 
     //Not enough arguments
     if(argc <= 1){
@@ -17,27 +32,29 @@ int main(int argc, char *argv[]){
     //File is specified
     if(argv[2]){
 
-        //Open file. If there is an error print error msg and exits w/ code 1
-        if((file = fopen(argv[2], "r"))==NULL){
-            printf("my-grep: cannot open file\n");
-            exit(1);
+        //Go through each file that is given
+        for(int filenum=2; filenum < argc; filenum++){
+
+            //Open file. If there is an error print error msg and exits w/ code 1
+            if((file = fopen(argv[filenum], "r"))==NULL){
+                printf("my-grep: cannot open file\n");
+                exit(1);
+            }
+
+            read_file(file, argv[1]);
         }
 
     //File is not specified
     }else{
+
+        //Set input to stdin
         file = stdin;
-    }
+        read_file(file, argv[1]);
 
-    //Go through each line of the file and find the search term
-    while(getline(&line, &len, file) != -1){
-        if(strstr(line, argv[1])){
-            printf("%s", line);
-        }
-    }
+        //If the program is ended with ctrl+d, no memory is leaked
+        //But if the program is ended with ctrl+c, the memory is leaked
 
-    //Close file and free line
-    fclose(file);
-    free(line);
+    }
 
     return 0;
 }
